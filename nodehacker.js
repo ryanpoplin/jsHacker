b3785a9f364a463b20b04ddc6c7689008c696731
Byrdann Fox <mobileupstate@gmail.com>
2014-07-16 09:55:39 -0400

    Updates...

---
+++ nodehacker.js
@@ -0,0 +1,29 @@
+(function(){
+
+	console.log("Node Testing...");
+
+	//
+	// what makes this Node API serious IP Property???
+	// what's the algorithm for IP/NDA agreement proposals for other developers???
+	// API, SDK, APP, Network, Security,  
+	// IP Property, Growth Potential, how much fucking money is it worth... 
+	//
+
+	//
+	// create a RESTful API with Node to intake data structure...
+	// the data 'interface' will need to accept two collections/arrays of data...
+	// the word object for each event... {date:new Date(),word: $('example').val()};
+	// [{date:new Date(),word:"I"},{date:new Date(),word:"want"},{date:new Date(), word:"coffee"}]; word objects for an entire day's date...
+	// [["I","want","coffee",{date:new Date()}],["You","are","cool",{date:new Date()}],["I","need","band-aid"],{date:new Date()}]; multi-dimenstional arrays...
+	//
+
+	//
+	// create a grid-collection with example illustrations and words with Backbone.js...
+	// create a the data analysis view with tables, charts, graphs, pies, (D3.js) etc...as a Backbone.js App...
+	// create a grid-collection/UICollectionView example with iOS with illustrations and words with Xcode...
+	// then do it in Android...
+	// 
+
+	
+
+}());
